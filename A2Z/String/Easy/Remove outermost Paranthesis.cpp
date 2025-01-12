954931c494b8dcf02d6e2cb432a931cf1a962e5a
Krutarth Pipaliya <118007834+Krutarth90@users.noreply.github.com>
2025-01-12 23:21:02 +0530

    Update Remove outermost Paranthesis.cpp

--- A2Z/String/Easy/Remove outermost Paranthesis.cpp
+++ A2Z/String/Easy/Remove outermost Paranthesis.cpp
@@ -1,5 +1,6 @@
 // https://leetcode.com/problems/remove-outermost-parentheses/
 
+// With stack
 class Solution {
 public:
     string removeOuterParentheses(string s) {
@@ -23,6 +24,46 @@ public:
                 st.pop();
                 if(!st.empty())
                     ans += s[i];
+            }
+        }
+        return ans;
+    }
+};
+
+
+// Without stack
+
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        bool f = 1;
+        int l = 0;
+        string ans = "";
+        for(int i = 0; i < (int) s.size(); i++)
+        {
+            if(s[i] == '(')
+            {
+                if(f)
+                {
+                    f = 0;
+                }
+                else
+                {
+                    l ++;
+                    ans += '(';
+                }
+            }
+            else 
+            {
+                if(l)
+                {
+                    l --;
+                    ans += ')';
+                }
+                else
+                {
+                    f = 1;
+                }
             }
         }
         return ans;
